1453a78321a5b2bdc51fbb614783cd285fe165bf
hzx <huang_zheng_xiang@163.com>
2025-03-17 23:34:38 +0800

    make SJTU app

--- apps/Android/MnnLlmChat/app/src/main/cpp/diffusion_session.cpp
+++ apps/Android/MnnLlmChat/app/src/main/cpp/diffusion_session.cpp
@@ -12,14 +12,12 @@ mls::DiffusionSession::DiffusionSession(std::string  resource_path): resource_pa
             resource_path_,
                           DiffusionModelType::STABLE_DIFFUSION_1_5,
                           MNNForwardType::MNN_FORWARD_OPENCL,
-                          1,
-                          20
-
+                          1
             );
     MNN_DEBUG("diffusion session init resource_path_: %s ", resource_path_.c_str());
     this->diffusion_->load();
 }
 
 void mls::DiffusionSession::Run(const std::string &prompt, const std::string &image_path, const std::function<void(int)>& progressCallback) {
-    this->diffusion_->run(prompt, image_path, progressCallback);
+    this->diffusion_->run(prompt, image_path, 20, 42, progressCallback);
 }
